1ea6b1dac15278772f500c16b8aad7c42cf3c172
Leonamanda <leonamanda828@gmail.com>
2026-05-05 22:36:30 +0200

    fix puerto render

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor en puerto " + PORT);
+});
+
+
 const express = require('express');
 const app = express();
 
