ceef31f69f7f5806da46254fbe60534c9c3295aa
Leonamanda <leonamanda828@gmail.com>
2026-05-11 11:38:50 +0200

    Fix login PostgreSQL

--- config/db.js
+++ config/db.js
@@ -1,15 +1,26 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+
+    connectionString:
+        process.env.DATABASE_URL,
+
     ssl: {
         rejectUnauthorized: false
     }
 });
 
 pool.connect()
-    .then(() => console.log('PostgreSQL conectado'))
-    .catch(err => console.log(err));
+    .then(() => {
+
+        console.log(
+            'PostgreSQL conectado'
+        );
+
+    })
+    .catch(err => {
+
+        console.log(err);
+    });
 
 module.exports = pool;
